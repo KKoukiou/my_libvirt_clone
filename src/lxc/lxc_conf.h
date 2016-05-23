a678d604a7f7dd650936fe3266044607ae6c3681
Katerina Koukiou <k.koukiou@gmail.com>
2016-05-23 14:02:52 +0300

    lxc: add migration support
    This patch adds initial migration support to the lxc driver,
    using the VIR_DRV_FEATURE_MIGRATION_PARAMS family of migration
    functions.
    This patch follows the pattern used in libxl driver.
    
    Signed-off-by: Katerina Koukiou <k.koukiou@gmail.com>

--- src/lxc/lxc_conf.h
+++ src/lxc/lxc_conf.h
@@ -34,12 +34,18 @@
 # include "security/security_manager.h"
 # include "configmake.h"
 # include "vircgroup.h"
+# include "virportallocator.h"
 # include "virsysinfo.h"
 # include "virusb.h"
 # include "virclosecallbacks.h"
 # include "virhostdev.h"
 
 # define LXC_DRIVER_NAME "LXC"
+
+# if WITH_CRIU
+#  define LXC_MIGRATION_PORT_MIN 49152
+#  define LXC_MIGRATION_PORT_MAX 49216
+# endif
 
 # define LXC_CONFIG_DIR SYSCONFDIR "/libvirt/lxc"
 # define LXC_STATE_DIR LOCALSTATEDIR "/run/libvirt/lxc"
@@ -61,6 +67,9 @@ struct _virLXCDriverConfig {
     char *logDir;
     int log_libvirtd;
     int have_netns;
+
+    /* Once created, caps are immutable */
+    virCapsPtr caps;
 
     char *securityDriverName;
     bool securityDefaultConfined;
@@ -104,6 +113,12 @@ struct _virLXCDriver {
 
     /* Immutable pointer, self-locking APIs */
     virCloseCallbacksPtr closeCallbacks;
+
+# if WITH_CRIU
+    /* Immutable pointer, self-locking APIs */
+    virPortAllocatorPtr migrationPorts;
+# endif
+
 };
 
 virLXCDriverConfigPtr virLXCDriverConfigNew(void);
