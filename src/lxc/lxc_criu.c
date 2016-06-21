250567d5b0c9e7504102e5a1edd2abd9e0d0ebea
Katerina Koukiou <k.koukiou@gmail.com>
2016-06-21 10:17:43 +0000

    lxc: adds checkpoint and restore support
    Use criu tool (https://criu.org/Main_Page) to add checkpoint/restore
    functionality for linux containers.
    This patch adds some wrapper functions for C/R that use libcriu,
    that is a C API for CRIU.
    
    Signed-off-by: Katerina Koukiou <k.koukiou@gmail.com>

---
+++ src/lxc/lxc_criu.c
@@ -0,0 +1,122 @@
+/*
+ * lxc_criu.c: wrapper functions for CRIU C API to be used for lxc migration
+ *
+ * Copyright (C) 2016 Katerina Koukiou
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Author: Katerina Koukiou <k.koukiou@gmail.com>
+ */
+
+#include <config.h>
+
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#include "virobject.h"
+#include "virerror.h"
+#include "virlog.h"
+#include "virfile.h"
+#include "lxc_domain.h"
+#include "lxc_driver.h"
+#include "lxc_criu.h"
+#include "criu/criu.h"
+
+#define VIR_FROM_THIS VIR_FROM_LXC
+
+VIR_LOG_INIT("lxc.lxc_criu");
+
+int lxcCriuDump(const char *checkpointdir,
+                virLXCDriverPtr driver ATTRIBUTE_UNUSED,
+                virDomainObjPtr vm)
+{
+    int fd;
+    int ret = -1;
+    virLXCDomainObjPrivatePtr priv;
+    char log[] = "dump.log";
+    char hugetlbfs[] = "hugetlbfs";
+    char tracefs[] = "tracefs";
+
+    if (mkdir(checkpointdir, 0777) < 0) {
+        virReportError(VIR_ERR_INTERNAL_ERROR,
+                       "%s", _("Can't create checkpoint directory"));
+        return -1;
+    }
+
+    fd = open(checkpointdir, O_DIRECTORY);
+    if (fd < 0) {
+        virReportSystemError(errno, "%s",
+                                   _("Failed to open directory"));
+        return -1;
+    }
+
+    if (criu_init_opts() == -1)
+        goto cleanup;
+
+    criu_set_images_dir_fd(fd);
+    criu_set_log_file(log);
+    criu_set_log_level(4);
+
+    priv = vm->privateData;
+    criu_set_pid(priv->initpid);
+    criu_set_tcp_established(true);
+    criu_set_file_locks(true);
+    criu_set_link_remap(true);
+    criu_set_force_irmap(true);
+    criu_set_manage_cgroups(true);
+    criu_set_auto_ext_mnt(true);
+    criu_add_enable_fs(hugetlbfs);
+    criu_add_enable_fs(tracefs);
+    criu_set_shell_job(true);
+    criu_set_leave_running(false);
+
+    VIR_DEBUG("About to checkpoint vm: %s pid=%d", vm->def->name, priv->initpid);
+    if (criu_dump() < 0)
+        return -1;
+
+    ret = 0;
+
+ cleanup:
+    VIR_FORCE_CLOSE(fd);
+    return ret;
+}
+
+int lxcCriuRestore(virDomainDefPtr def, int fd)
+{
+    int ret = -1;
+    char log[] = "restore.log";
+    virDomainFSDefPtr root;
+
+    root = virDomainGetFilesystemForTarget(def, "/");
+
+    criu_init_opts();
+
+    criu_set_images_dir_fd(fd);
+    criu_set_log_file(log);
+    criu_set_log_level(4);
+
+    criu_set_auto_ext_mnt(true);
+    criu_set_ext_sharing(true);
+    criu_set_ext_masters(true);
+    criu_set_file_locks(true);
+    criu_set_shell_job(true);
+
+    criu_set_manage_cgroups(true);
+    /* Change the root filesystem (when run in mount namespace) */
+    criu_set_root(root->src);
+    ret = criu_restore();
+
+    return ret;
+}
